ffcb5622ae15a84ea1c93feb603417dc63d45991
tjaulin <thomas.jln27@gmail.com>
2022-03-25 14:46:55 +0100

    Initial commit

---
+++ src/main/java/fr/sdv/b32122/App.java
@@ -0,0 +1,13 @@
+package fr.sdv.b32122;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/fr/sdv/b32122/bo/Product.java
@@ -0,0 +1,2 @@
+package fr.sdv.b32122.bo;public class Product {
+}
